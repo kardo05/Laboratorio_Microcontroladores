81be5a2065c10dea9adad00d10755f2c09c534c0
Wilber Hernandez <11430410+wilberahr@users.noreply.github.com>
2024-05-08 15:52:19 -0600

    elminacion de pid.h y pid.c y se agregaron sus funciones directamente en incubadora.ino

--- Labo3/src/incubadora/incubadora.ino
+++ Labo3/src/incubadora/incubadora.ino
@@ -1,29 +1,52 @@
 #include <PCD8544.h>
-#include "pid.h"
+
 #include "simPlanta.h"
+
+struct PIDControlador{
+
+    float Kp;
+    float Ki;
+    float Kd;
+
+
+    float posicion_actual;
+    float error;
+    float integral;
+    float derivada;
+    float var_control;
+    float ultimo_error;
+
+
+};
+
+
+void PIDControlador_Init(PIDControlador *pid);
+float PIDControlador_Update(PIDControlador *pid, float setpoint, float posicion_actual);
+
 
 PCD8544 pantalla;
 PIDControlador controlador;
+PIDControlador *pid = &controlador;
 
-float Kp;
-float Ki;
-float kd;
+pid.Kp = 1;
+pid.Ki = 1;
+pid.Kd = 1;
 
 float setpoint;
 float temperatura;
-float temperatura_ambiente;
+float temperatura_ambiente = 25;
 
 int baudrate = 9600;
-PCD8544 pantalla;
-PIDControlador controlador;
-double 
+int potenciometro = A0;
 
 
 void setup()
 {
-	pantalla.begin()
+	PIDControlador_Init(pid);
+    pantalla.begin();
     Serial.begin(baudrate);
-    temperatura = PIDControlador_Update(&controlador, setpoint, temperatura_ambiente);
+    setpoint = analogRead(potenciometro)*12/1000 +30;
+    temperatura = PIDControlador_Update(pid, setpoint, temperatura_ambiente);
 }
 
 void loop()
@@ -31,7 +54,49 @@ void loop()
 	pantalla.clear();
     float TempWatts = temperatura * 20.0 / 255;
     temperatura = simPlanta(TempWatts);
-    temperatura = PIDControlador_Update(&controlador, setpoint, temperatura);
+    setpoint = analogRead(potenciometro)*12/1000 +30;
+    temperatura = PIDControlador_Update(pid, setpoint, temperatura);
     
 
 }
+  
+void PIDControlador_Init(PIDControlador *pid){
+
+    pid.integral = 0.0;
+    pid.ultimo_error = 0.0;
+    pid.derivada = 0.0;
+    pid.posicion_actual = 0.0;
+
+    pid.var_control = 0.0;
+
+}
+  
+float PIDControlador_Update(PIDControlador *pid, float set_point, float medicion) {
+
+
+    //Leer entrada
+    pid.posicion_actual = medicion;
+    //Calcular el error
+    pid.error = set_point - pid.posicion_actual ;
+    //Calcular la integral
+    pid.integral = pid.integral + pid.error ;
+    //Calcular la derivada
+    pid.derivada = pid.error - pid.ultimo_error ;
+    //Calcular variable de control
+    pid.var_control = (pid.Kp * pid.error ) + ( pid.Ki * pid.integral ) + (pid.Kd * pid.derivada ) ;
+    //Acotar variable de control
+    if( pid.var_control > 255){
+        pid.var_control = 255;
+    }    
+    else{
+        if( pid.var_control < 0){
+            pid.var_control = 0;
+            
+        }
+    }
+    pid.ultimo_error = pid.error; 
+    return pid.var_control;    
+}
+
+    
+    
