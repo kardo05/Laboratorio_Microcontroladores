7b812936a77da2997442de154e5016aa2894186d
Wilber Hernandez <11430410+wilberahr@users.noreply.github.com>
2024-05-08 16:05:47 -0600

    actualizacion de incubadora.ino y primer build sin errores con la funcion de control PID. Se agrega funcion simPlanta al archivo incubadora.ino

--- Labo3/src/build/sketch/pid.c.d
+++
@@ -1,3 +0,0 @@
-/home/wilberahr/Documents/Laboratorio_Microcontroladores/Labo3/src/build/sketch/pid.c.o: \
- /home/wilberahr/Documents/Laboratorio_Microcontroladores/Labo3/src/build/sketch/pid.c \
- /home/wilberahr/Documents/Laboratorio_Microcontroladores/Labo3/src/build/sketch/pid.h
--- Labo3/src/incubadora/incubadora.ino
+++ Labo3/src/incubadora/incubadora.ino
@@ -1,6 +1,6 @@
 #include <PCD8544.h>
 
-#include "simPlanta.h"
+//#include "simPlanta.h"
 
 struct PIDControlador{
 
@@ -28,9 +28,7 @@ PCD8544 pantalla;
 PIDControlador controlador;
 PIDControlador *pid = &controlador;
 
-pid.Kp = 1;
-pid.Ki = 1;
-pid.Kd = 1;
+
 
 float setpoint;
 float temperatura;
@@ -42,7 +40,11 @@ int potenciometro = A0;
 
 void setup()
 {
-	PIDControlador_Init(pid);
+	pid->Kp = 1;
+    pid->Ki = 1;
+    pid->Kd = 1;
+
+    PIDControlador_Init(pid);
     pantalla.begin();
     Serial.begin(baudrate);
     setpoint = analogRead(potenciometro)*12/1000 +30;
@@ -62,12 +64,12 @@ void loop()
   
 void PIDControlador_Init(PIDControlador *pid){
 
-    pid.integral = 0.0;
-    pid.ultimo_error = 0.0;
-    pid.derivada = 0.0;
-    pid.posicion_actual = 0.0;
+    pid->integral = 0.0;
+    pid->ultimo_error = 0.0;
+    pid->derivada = 0.0;
+    pid->posicion_actual = 0.0;
 
-    pid.var_control = 0.0;
+    pid->var_control = 0.0;
 
 }
   
@@ -75,28 +77,47 @@ float PIDControlador_Update(PIDControlador *pid, float set_point, float medicion
 
 
     //Leer entrada
-    pid.posicion_actual = medicion;
+    pid->posicion_actual = medicion;
     //Calcular el error
-    pid.error = set_point - pid.posicion_actual ;
+    pid->error = set_point - pid->posicion_actual ;
     //Calcular la integral
-    pid.integral = pid.integral + pid.error ;
+    pid->integral = pid->integral + pid->error ;
     //Calcular la derivada
-    pid.derivada = pid.error - pid.ultimo_error ;
+    pid->derivada = pid->error - pid->ultimo_error ;
     //Calcular variable de control
-    pid.var_control = (pid.Kp * pid.error ) + ( pid.Ki * pid.integral ) + (pid.Kd * pid.derivada ) ;
+    pid->var_control = (pid->Kp * pid->error ) + ( pid->Ki * pid->integral ) + (pid->Kd * pid->derivada ) ;
     //Acotar variable de control
-    if( pid.var_control > 255){
-        pid.var_control = 255;
+    if( pid->var_control > 255){
+        pid->var_control = 255;
     }    
     else{
-        if( pid.var_control < 0){
-            pid.var_control = 0;
+        if( pid->var_control < 0){
+            pid->var_control = 0;
             
         }
     }
-    pid.ultimo_error = pid.error; 
-    return pid.var_control;    
+    pid->ultimo_error = pid->error; 
+    return pid->var_control;    
 }
 
-    
+float simPlanta(float Q) {
+    // simula un bloque de aluminio de 1x1x2cm con un calentador y con enfreamiento pasivo
+    // float C = 237; // W/mK coeficiente de conductividad termica para el Aluminio
+    float h = 5; // W/m2K coeficiente de conveccion termica para el Aluminio
+    float Cps = 0.89; // J/gC
+    float area = 1e-4; // m2 area por conveccion
+    float masa = 10 ; // g
+    float Tamb = 25; // Temperatura ambiente en C
+    float T = Tamb; // Temperatura en C
+    static uint32_t last = 0;
+    uint32_t interval = 100; // ms
+
+    if ( millis () - last >= interval) {
+        last += interval;
+        // 0−transferencia de calor
+        T = T + Q * interval / 1000 / masa / Cps - (T - Tamb) * area * h;
+    }
+
+    return T;
+}
     
