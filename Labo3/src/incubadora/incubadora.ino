1c20f7943b97c77c222d532f58d3d0c665cd54a5
Ricardo Hidalgo <118785158+kardo05@users.noreply.github.com>
2024-05-08 18:19:04 -0600

    Creacion de la estructura logica, falta definir variables y el setup

--- Labo3/src/incubadora/incubadora.ino
+++ Labo3/src/incubadora/incubadora.ino
@@ -4,11 +4,17 @@
 
 PCD8544 pantalla;
 PIDControlador controlador;
-
-float Kp;
-float Ki;
-float kd;
-
+int verde      = 11;
+int azul       = 12;
+int rojo       = 13;
+potenciometro  = 0;
+potenciometro1 = 0;
+int pot_pin    = A0;
+int cambio     = 8;
+int transistor = 9;
+float Kp       = 2;
+float Ki       = 5;
+float kd       = 1;
 float setpoint;
 float temperatura;
 float temperatura_ambiente;
@@ -32,6 +38,46 @@ void loop()
     float TempWatts = temperatura * 20.0 / 255;
     temperatura = simPlanta(TempWatts);
     temperatura = PIDControlador_Update(&controlador, setpoint, temperatura);
-    
+    float senal = 1;
+    potenciometro = analogRead(pot_pin)*12/1000 +30;
+    pottenciometro1= (potenciometro-30)/12*255;
 
+     input=temp;
+    if (setpoint != potenciometro){
+    setpoint=potenciometro;     
+    }
+    senal=output/255*80;
+    analogWrite(transistor, output);
+    lcd.setCursor(0, 0);
+    lcd.print("T_o: ");
+    lcd.print(potenciometro);
+    lcd.print(" *C");
+    lcd.setCursor(0,1);
+    lcd.print("Senal: ");
+    lcd.print(senal);
+    lcd.print("*C");  
+    lcd.setCursor(0, 2);
+    lcd.print("T_s: ");
+    lcd.print(temp);
+    lcd.print(" *C ");    
+
+    if (temp <= 30){
+     digitalWrite(azul, HIGH);
+    }
+    else {
+        digitalWrite(azul, LOW);
+    }
+    if (temp >= 42){
+        digitalWrite(rojo, HIGH);
+    }
+    else {
+        digitalWrite(rojo, LOW);
+    }
+    if (30 <= temp <= 42){
+        digitalWrite(verde, HIGH);
+    }
+    else {
+        digitalWrite(verde, LOW);
+    }
 }
+    delay(2000);
