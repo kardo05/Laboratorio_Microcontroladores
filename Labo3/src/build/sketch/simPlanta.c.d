484aa100b6e4fd58d1e7dc5e1415330209499bc9
Wilber Hernandez <11430410+wilberahr@users.noreply.github.com>
2024-05-28 04:58:33 -0600

    Actualizacion de repositorio para permitir avanzar con laboratorio 4.

--- Labo3/src/build/sketch/simPlanta.c.d
+++
@@ -1,2 +0,0 @@
-/home/wilberahr/Documents/Laboratorio_Microcontroladores/Labo3/src/build/sketch/simPlanta.c.o: \
- /home/wilberahr/Documents/Laboratorio_Microcontroladores/Labo3/src/build/sketch/simPlanta.c
